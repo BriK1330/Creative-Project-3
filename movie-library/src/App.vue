f0b3c81902befe7c57e13490a94586f109bb03ce
Briana King <brianaking1330@gmail.com>
2021-11-06 01:03:56 -0600

    Fixing /s

--- movie-library/src/App.vue
+++ movie-library/src/App.vue
@@ -3,7 +3,7 @@
   <div id="menu">
     <div id="brand">
       <router-link to="/">
-        <img src="..\images\logo.png">
+        <img src="../images/logo.png">
       </router-link>
     </div>
     <div id="side">
